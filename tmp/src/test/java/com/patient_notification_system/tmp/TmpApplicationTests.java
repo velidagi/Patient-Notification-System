326c0d31bc76c984fa33656ad815e092a3ccb865
velydagi <velydagi@gmail.com>
2024-07-11 23:18:12 +0300

    create: spring boot system

---
+++ tmp/src/test/java/com/patient_notification_system/tmp/TmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patient_notification_system.tmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
